9ef4cf45a809169077e391d55495272f27c8f98a
Leonardo Camelo <leonardocamelo20@gmail.com>
2026-02-10 12:49:05 -0300

    Feature: userController atualizado

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,37 +2,47 @@ import { prisma } from "../lib/prisma.js";
 
 export const listUsers = async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const search = req.query.search || "";
-    const skip = (page - 1) * limit;
-
-    const where = search
-      ? {
-          OR: [
-            { name: { contains: search, mode: "insensitive" } },
-            { username: { contains: search, mode: "insensitive" } },
-          ],
-        }
-      : {};
-
-    const users = await prisma.user.findMany({
-      where,
-      select: {
-        id: true,
-        username: true,
-        displayName: true,
-      },
-      skip: skip,
-      take: limit,
-    });
-
+    const users = await prisma.user.findMany(); // Sem filtros, sem segredos
     res.status(200).json(users);
   } catch (error) {
-    console.error("ERRO NO PRISMA:", error);
-    res.status(500).json({ error: "Erro ao buscar usuários" });
+    console.error("ERRO DETALHADO:", error);
+    res.status(500).json({ error: error.message }); // Isso vai mostrar o erro real no JSON
   }
 };
+
+// export const listUsers = async (req, res) => {
+//   try {
+//     const page = Number(req.query.page) || 1;
+//     const limit = Number(req.query.limit) || 10;
+//     const search = req.query.search || "";
+//     const skip = (page - 1) * limit;
+
+//     const where = search
+//       ? {
+//           OR: [
+//             { name: { contains: search, mode: "insensitive" } },
+//             { username: { contains: search, mode: "insensitive" } },
+//           ],
+//         }
+//       : {};
+
+//     const users = await prisma.user.findMany({
+//       where,
+//       select: {
+//         id: true,
+//         username: true,
+//         displayName: true,
+//       },
+//       skip: skip,
+//       take: limit,
+//     });
+
+//     res.status(200).json(users);
+//   } catch (error) {
+//     console.error("ERRO NO PRISMA:", error);
+//     res.status(500).json({ error: "Erro ao buscar usuários" });
+//   }
+// };
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
